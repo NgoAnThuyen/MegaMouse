5ef47b03f8153e5a79ff0a483577db2bbe869cd2
Ngô An Thuyên <36561426+NgoAnThuyen@users.noreply.github.com>
2023-11-04 20:06:16 +0700

    Add files via upload

---
+++ Code/megaMouse_10.ino
@@ -0,0 +1,237 @@
+/*-------------------------------VL530XV2--------------------------*/
+#include <Wire.h>
+#include <VL53L0X.h>
+const uint8_t sensorCount = 3;
+const uint8_t xshutPins[sensorCount] = {4,2,15};
+VL53L0X sensors[sensorCount];
+/*-------------------------------PWM--------------------------*/
+const int ledPin1 = 25; 
+const int ledPin2 = 13;  
+// configuration of PWM channels
+const int freq = 5000;  // tần số xung
+const int ledChannel1 = 0; // kênh PWM
+const int ledChannel2 = 1; // kênh PWM
+const int resolution = 8; // độ phân giải 8bit
+/*-------------------------------MOTOR DIRECTION--------------------------*/
+#define DIR1_1 27
+#define DIR1_2 26
+#define DIR2_1 12
+#define DIR2_2 14
+/*-------------------------------GLOBAL VARIABLES--------------------------*/
+unsigned long previousMillis = 0;   
+long thetaLeft, thetaRight, previousThetaLeft, previousThetaRight;
+int sensorLeft, sensorMid, sensorRight ;
+bool running = false;
+int previousState;
+bool busy;
+bool hehe= false;
+int cnt;
+float baseFwd, turn;
+float desiredLeftDuty, leftDuty, previousLeftDuty;
+float desiredRightDuty, rightDuty, previousRightDuty;
+float sumErrorLeft, previousErrorLeft;
+int previousSensorLeft;
+int cntTurn180;
+/*-------------------------------ROBOT PARAMETERS--------------------------*/
+
+void setup()
+{  
+  Serial.begin(115200);
+
+  Wire.begin();
+  Wire.setClock(400000); // use 400 kHz I2C
+
+   // Disable/reset all sensors by driving their XSHUT pins low.
+  for (uint8_t i = 0; i < sensorCount; i++)
+  {
+    pinMode(xshutPins[i], OUTPUT);
+    digitalWrite(xshutPins[i], LOW);
+  }
+  // Enable, initialize, and start each sensor, one by one.
+  for (uint8_t i = 0; i < sensorCount; i++)
+  {
+    digitalWrite(xshutPins[i], HIGH);
+    // pinMode(xshutPins[i], INPUT_PULLUP);
+    delay(100);
+    sensors[i].setTimeout(500);
+    if (!sensors[i].init())
+    {
+      Serial.print("Failed to detect and initialize sensor ");
+      Serial.println(i);
+      while (1);
+    }
+    sensors[i].setAddress(0x50 + i);
+    // sensors[i].setMeasurementTimingBudget(20000);
+    sensors[i].startContinuous();
+  }
+  delay(500);
+  ledcSetup(ledChannel1, freq, resolution);
+  ledcSetup(ledChannel2, freq, resolution);
+  ledcAttachPin(ledPin1, ledChannel1);
+  ledcAttachPin(ledPin2, ledChannel2);
+
+  pinMode(DIR1_1, OUTPUT);
+  pinMode(DIR1_2, OUTPUT);
+  pinMode(DIR2_1, OUTPUT);
+  pinMode(DIR2_2, OUTPUT);
+  running = false;
+  busy = true;
+}
+void loop() {
+  unsigned long nowMillis = millis();  
+  if (nowMillis - previousMillis >= 100) // 50 ms LOOP
+  {
+    sensorLeft = sensors[0].readRangeContinuousMillimeters();
+    sensorMid = sensors[1].readRangeContinuousMillimeters();
+    sensorRight = sensors[2].readRangeContinuousMillimeters() - 55;
+    
+    sensorLeft = constrain(sensorLeft,30, 250);
+    sensorMid = constrain(sensorMid,30, 250);
+    sensorRight = constrain(sensorRight,30, 250);
+    if (sensorMid < 40) running = true;
+  Serial.print("L:"); Serial.print(sensorLeft); Serial.print('\t');
+  Serial.print("M: "); Serial.print(sensorMid); Serial.print('\t');
+  Serial.print("R: "); Serial.print(sensorRight); Serial.print('\t');
+
+  int state = getWallState(sensorLeft, sensorMid, sensorRight, 75);
+  if ((sensorLeft - previousSensorLeft) >= 100) hehe = true;
+  Serial.print("state "); Serial.print(state); Serial.print('\t');
+  Serial.print("hehe "); Serial.print(hehe); Serial.print('\t');
+  if (running)
+    {
+      if (!hehe)
+      {
+        if (state == 0)
+        {
+          turn = PID_LeftWall(sensorLeft,70.0,2.5,0.0,0.1,0.05);
+          Serial.print(" PID LEFT ");
+          // Serial.print(errorLeft);
+          baseFwd = 200;
+        }
+        else if (state == 1)
+        {
+          sumErrorLeft = 0;
+          baseFwd = 0;
+          turn = 200;
+        }
+      }
+      else 
+      {
+        sumErrorLeft = 0;
+        cnt+=1;
+        if (cnt <= 8) // tuning  number 10 for other condition // timming
+        {
+          turn = 0;
+          baseFwd = 200;
+          Serial.print(" Straight");
+        }
+        else if (cnt <= 10) // tuning  number 15 for other condition /timming
+        {
+          baseFwd = 0;
+          turn = -100;          
+          Serial.print(" Turn Left with holding position ");
+        }
+        else 
+        {
+          baseFwd = 150;
+          turn = -100;
+          Serial.print(" Turn Left");
+        }
+
+        if (sensorLeft <= 110)
+        {
+          cnt = 0;
+          hehe = false;
+          baseFwd = 0;
+          turn = 0;
+        }
+      }
+
+        leftDuty = baseFwd + turn;
+        rightDuty = baseFwd - turn;
+        leftDuty = constrain(leftDuty,-250.0,250.0);
+        rightDuty = constrain(rightDuty,-250.0,250.0);
+        // Serial.print(running);
+        Left_Motor_PWM((int)leftDuty);
+        Right_Motor_PWM((int)rightDuty);
+
+    }
+
+    Serial.println();
+    previousSensorLeft = sensorLeft;
+    previousMillis = nowMillis;
+  }
+
+}
+int getWallState(int sensorLeft, int sensorMid, int sensorRight, int threshold)
+{
+  // return from 0 to 8 corresponding to 000 -> 111, 1 means wall.
+  int wallState;
+  // int thresholdLeft  = (float)threshold*1.41;
+  // int thresholdRight = (float)threshold*1.41;
+  // if      ((sensorLeft >= thresholdLeft) && (sensorMid >= threshold) && (sensorRight >= thresholdRight))
+  //   wallState = 0;
+  // else if ((sensorLeft >= thresholdLeft) && (sensorMid >= threshold) && (sensorRight < thresholdRight))
+  //   wallState = 1;
+  // else if ((sensorLeft >= thresholdLeft) && (sensorMid < threshold) && (sensorRight >= thresholdRight))
+  //   wallState = 2;    
+  // else if ((sensorLeft >= thresholdLeft) && (sensorMid < threshold) && (sensorRight < thresholdRight))
+  //   wallState = 3;
+  // else if ((sensorLeft < thresholdLeft) && (sensorMid >= threshold) && (sensorRight >= thresholdRight))
+  //   wallState = 4;
+  // else if ((sensorLeft < thresholdLeft) && (sensorMid >= threshold) && (sensorRight < thresholdRight))
+  //   wallState = 5;    
+  // else if ((sensorLeft < thresholdLeft) && (sensorMid < threshold) && (sensorRight >= thresholdRight))
+  //   wallState = 6;
+  // else if ((sensorLeft < thresholdLeft) && (sensorMid < threshold) && (sensorRight < thresholdRight))
+  //   wallState = 7;    
+
+  if (sensorMid > threshold)
+    wallState = 0;
+  else
+    wallState = 1;
+
+  return wallState;
+}
+
+float PID_LeftWall(int sensorLeft, float desiredSensorLeft,float Kp, float Ki, float Kd, float Ts)
+{
+  float errorLeft = desiredSensorLeft - (float)sensorLeft;
+  sumErrorLeft += errorLeft;
+  float turn_= Kp*errorLeft + Ki*sumErrorLeft*Ts + Kd* (errorLeft - previousErrorLeft)/Ts;
+  previousErrorLeft = errorLeft;
+  return turn_;
+}
+
+void Left_Motor_PWM(float PWM_)
+{
+  if (PWM_ >=0) 
+  {
+    ledcWrite(ledChannel1, PWM_);
+    digitalWrite(DIR1_1, LOW);
+    digitalWrite(DIR1_2, HIGH);
+  }
+  else
+  {
+    ledcWrite(ledChannel1, abs(PWM_));
+    digitalWrite(DIR1_1, HIGH);
+    digitalWrite(DIR1_2, LOW);
+  }
+}
+
+void Right_Motor_PWM(float PWM_)
+{
+  if (PWM_ >=0) 
+  {
+    ledcWrite(ledChannel2, PWM_);
+    digitalWrite(DIR2_1, HIGH);
+    digitalWrite(DIR2_2, LOW);
+  }
+  else
+  {
+    ledcWrite(ledChannel2, abs(PWM_));
+    digitalWrite(DIR2_1, LOW);
+    digitalWrite(DIR2_2, HIGH);
+  }
+}
+
